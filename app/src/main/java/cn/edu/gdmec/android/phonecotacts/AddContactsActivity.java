4b38a352914bbbd23539276624b4fdbfe97eaff0
MelaJia <838672863@qq.com>
2018-05-07 19:50:03 +0800

    添加联系人

--- app/src/main/java/cn/edu/gdmec/android/phonecotacts/AddContactsActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/phonecotacts/AddContactsActivity.java
@@ -1,9 +1,80 @@
 package cn.edu.gdmec.android.phonecotacts;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
 /**
  * Created by HP on 2018/4/22.
  * 显示添加数据界面
  */
 
-public class AddContactsActivity {
+public class AddContactsActivity extends AppCompatActivity {
+    private EditText name;
+    private EditText danwei;
+    private EditText mobile;
+    private EditText qq;
+    private EditText address;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_edit);
+        initView();
+
+    }
+
+    private void initView() {
+        name = (EditText) findViewById(R.id.name);
+        danwei = (EditText) findViewById(R.id.danwei);
+        mobile = (EditText) findViewById(R.id.mobile);
+        qq = (EditText) findViewById(R.id.qq);
+        address = (EditText) findViewById(R.id.address);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE,1,Menu.NONE,"保存");
+        menu.add(Menu.NONE,2,Menu.NONE,"返回");
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case 1:
+                if (!name.getText().toString().equals("")){
+                    User user=new User();
+                    user.setName(name.getText().toString());
+                    user.setMobile(mobile.getText().toString());
+                    user.setQq(qq.getText().toString());
+                    user.setDanwei(danwei.getText().toString());
+                    user.setAddress(address.getText().toString());
+                    ContactsTable ct=new ContactsTable(AddContactsActivity.this);
+                    if (ct.addData(user)){
+                        Toast.makeText(this, "添加成功！", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }else {
+                        Toast.makeText(this, "添加失败！", Toast.LENGTH_SHORT).show();
+
+                    }
+                }else {
+                    Toast.makeText(this, "请先输入数据", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 2:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/cn/edu/gdmec/android/phonecotacts/ContactsTable.java
+++ app/src/main/java/cn/edu/gdmec/android/phonecotacts/ContactsTable.java
@@ -1,8 +1,11 @@
 package cn.edu.gdmec.android.phonecotacts;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 
 import java.io.PushbackInputStream;
+import java.util.Vector;
 
 /**
  * Created by apple on 18/4/17.
@@ -25,20 +28,143 @@ public  class ContactsTable {
                     User.DANWEI+"VARCHAR," +
                     User.ADDRESS+"VARCHAR" +
                     ")";
+            db.createTable(createTableSql);
 
         }
 
    }
+   public boolean addData(User user){
+       ContentValues values=new ContentValues();
+       values.put(User.NAME,user.getName());
+       values.put(User.MOBILE,user.getMobile());
+       values.put(User.DANWEI,user.getDanwei());
+       values.put(User.QQ,user.getQq());
+       values.put(User.ADDRESS,user.getAddress());
+       return db.save(TABLENAME,values);
 
+   }
+//获取联系人表数据
     public User[] getAllUser() {
-        return new User[0];
-    }
-    public User[] findUserByKey(String key){
-        return new User[0];
+       //定义一个专门存放User类型数据Vector的向量类对象
+        Vector<User> v=new Vector<User>();
+        Cursor cursor=null;
+        try {
+            cursor=db.find("select * from "+TABLENAME,null);
+            while (cursor.moveToNext()) {
+                User temp = new User();
+                temp.setId_DB(cursor.getInt(cursor.getColumnIndex("id_DB")));
+                temp.setName(cursor.getString(cursor.getColumnIndex(User.NAME)));
+                temp.setAddress(cursor.getString(cursor.getColumnIndex(User.ADDRESS)));
+                temp.setDanwei(cursor.getString(cursor.getColumnIndex(User.DANWEI)));
+                temp.setQq(cursor.getString(cursor.getColumnIndex(User.DANWEI)));
+                temp.setMobile(cursor.getString(cursor.getColumnIndex(User.MOBILE)));
+                //将符合查询条件的User对象添加到向量集合中
+                v.add(temp);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (cursor!=null){
+                cursor.close();
 
+            }
+            db.closeConnection();
+        }
+        if (v.size()>0){
+            //将vector中的数据，强制以User数组形式返回
+            return v.toArray(new User[] {});
+
+        }else {
+            User[] users=new User[1];
+            User user=new User();
+            user.setName("无结果");
+            users[0]=user;
+            return users;
+
+        }
+    }
+//    根据数据库改变主键ID来获取联系人
+    public User getUserByID(int id){
+       Cursor cursor=null;
+       try {
+           cursor=db.find("select * from "+TABLENAME+" where "
+           +"id_DB=?",new String[]{id+""});
+           User temp=new User();
+           cursor.moveToNext();
+           temp.setId_DB(cursor.getInt(cursor.getColumnIndex("id_DB")));
+           temp.setName(cursor.getString(cursor.getColumnIndex(User.NAME)));
+           temp.setAddress(cursor.getString(cursor.getColumnIndex(User.ADDRESS)));
+           temp.setDanwei(cursor.getString(cursor.getColumnIndex(User.DANWEI)));
+           temp.setQq(cursor.getString(cursor.getColumnIndex(User.DANWEI)));
+           temp.setMobile(cursor.getString(cursor.getColumnIndex(User.MOBILE)));
+           return temp;
+       }catch (Exception e)
+       {
+           e.printStackTrace();
+       }finally {
+           if (cursor!=null){
+               cursor.close();
+           }
+           db.closeConnection();
+       }
+       return null;
+    }
+
+    public User[] findUserByKey(String key){
+        Vector<User> v=new Vector<User>();
+        Cursor cursor=null;
+        try {
+            cursor=db.find("select * from "+TABLENAME+" where "
+                    +User.NAME+" like '%"+key+"%'"+
+                    " or "+User.MOBILE+" like '%"+key+"%'"+
+                    " or "+User.MOBILE+" like '%"+key+"%'",null);
+            while (cursor.moveToNext()) {
+                User temp = new User();
+                temp.setId_DB(cursor.getInt(cursor.getColumnIndex("id_DB")));
+                temp.setName(cursor.getString(cursor.getColumnIndex(User.NAME)));
+                temp.setAddress(cursor.getString(cursor.getColumnIndex(User.ADDRESS)));
+                temp.setDanwei(cursor.getString(cursor.getColumnIndex(User.DANWEI)));
+                temp.setQq(cursor.getString(cursor.getColumnIndex(User.DANWEI)));
+                temp.setMobile(cursor.getString(cursor.getColumnIndex(User.MOBILE)));
+                //将符合查询条件的User对象添加到向量集合中
+                v.add(temp);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (cursor!=null){
+                cursor.close();
+
+            }
+            db.closeConnection();
+        }
+        if (v.size()>0){
+            //将vector中的数据，强制以User数组形式返回
+            return v.toArray(new User[] {});
+
+        }else {
+            User[] users=new User[1];
+            User user=new User();
+            user.setName("无结果");
+            users[0]=user;
+            return users;
+
+        }
+
+    }
+    public boolean updateUser(User user){
+        ContentValues values=new ContentValues();
+        values.put(User.NAME,user.getName());
+        values.put(User.MOBILE,user.getMobile());
+        values.put(User.DANWEI,user.getDanwei());
+        values.put(User.QQ,user.getQq());
+        values.put(User.ADDRESS,user.getAddress());
+
+
+        return db.update(TABLENAME,values," id_DB=?",new String[]{user.getId_DB()+""});
     }
 
     public boolean deleteByUser(User user) {
-        return false;
+        return db.delete(TABLENAME," id_DB=?",new String[]{user.getId_DB()+""});
     }
 }
